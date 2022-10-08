e711fa0317a12ef6f4bcd7eb5cddb7b3ac350c71
Grace X Wang <grace.wang95@gmail.com>
2022-10-07 17:38:48 -0700

    add Face Recognition component

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaceRecognition = () => {
+    return (
+        <div className="center">
+            <img alt='image to process' src={ 'https://samples.clarifai.com/metro-north.jpg' }/>
+        </div>
+    );
+}
+
+export default FaceRecognition;
